ce90fca8ca62cb344e9beb9ae508e374ccccb6dd
EGPdeveloper <estefaniagopre@gmail.com>
2022-11-05 14:00:21 -0500

    [Tarea] Primer React JSX

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>Learn React</ul>
+        <ul>Do homeworks Coding Dojo</ul>
+        <ul>Do excercise</ul>
+        <ul>Clean the House</ul>
+        <ul>Visit Grandmothers</ul>
+      </div>
+      
+    );
+  }
+}
+
+
+
+export default App;
